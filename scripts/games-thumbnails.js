f110dfba76822f3e77c5191e7ba2e688337dc46f
Triple the Boba ! <coconuttt1010@gmail.com>
2023-10-27 19:42:20 -0700

    update boilerplate

---
+++ scripts/games-thumbnails.js
@@ -0,0 +1,4 @@
+// function to load games thumbnails in main homepage
+$(function () {
+  $("#games-thumbnails").load("games-thumbnails.html");
+});
---
+++ scripts/shared-elements.js
@@ -0,0 +1,11 @@
+/** load shared element across all pages */
+
+// function to load nav bar to reuse
+$(function () {
+  $("#navbar").load("navbar.html");
+});
+
+// function to load nav bar to reuse
+$(function () {
+  $("#footer").load("footer.html");
+});
