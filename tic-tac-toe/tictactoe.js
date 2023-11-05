1a5e56cea7a47d0789db95dbe594aa79d52ef43e
Triple the Boba ! <coconuttt1010@gmail.com>
2023-11-04 19:26:36 -0700

    specify tictactoe style targets only itself, set header width global

--- tic-tac-toe/tictactoe.js
+++ tic-tac-toe/tictactoe.js
@@ -1,4 +1,4 @@
-const statusDisplay = document.querySelector(".game--status");
+const statusDisplay = document.querySelector(".status");
 
 let gameActive = true;
 let currentPlayer = "X";
@@ -84,6 +84,4 @@ function handleRestartGame() {
 document
   .querySelectorAll(".cell")
   .forEach((cell) => cell.addEventListener("click", handleCellClick));
-document
-  .querySelector(".game--restart")
-  .addEventListener("click", handleRestartGame);
+document.querySelector(".restart").addEventListener("click", handleRestartGame);
