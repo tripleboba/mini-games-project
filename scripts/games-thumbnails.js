a97f15c6f7c9249c8755a457a860ddf39966b162
Triple the Boba ! <coconuttt1010@gmail.com>
2023-11-05 19:36:36 -0800

    render thumbnails into home page, correct css , correct top navbar and footer

--- scripts/games-thumbnails.js
+++
@@ -1,4 +0,0 @@
-// function to load games thumbnails in main homepage
-$(function () {
-  $("#games-thumbnails").load("game-thumbnails/games-thumbnails.html");
-});
---
+++ scripts/home.js
@@ -0,0 +1,5 @@
+$(function () {
+  $("#topNavbar").load("top-navbar/top-navbar.html");
+  $("#game-thumbnails").load("game-thumbnails/game-thumbnails.html");
+  $("#footer").load("footer/footer.html");
+});
